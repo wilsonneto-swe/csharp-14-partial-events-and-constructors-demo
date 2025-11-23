e04f7779b0cdd46bdf1d6f1c54be5e7dd2bb011d
Wilson Gomes <20674439+wilsongomes-swe@users.noreply.github.com>
2025-11-22 21:02:31 -0300

    add demo code

---
+++ PartialConstructors/Program.cs
@@ -0,0 +1,21 @@
+﻿var user = new User("A User Name");
+user.NameChanged += name => Console.WriteLine($"Listener: {name}");
+
+Console.WriteLine($"Current name is: {user.Name}");
+
+user.Rename("A New Name");
+
+Console.WriteLine($"Current name is: {user.Name}");
+
+    
+[AutoUser]
+public partial class User
+{
+    public string Name { get; private set; }
+
+    public partial User(string name);
+
+    public partial event Action<string> NameChanged;
+
+    public partial void Rename(string newName);
+}
